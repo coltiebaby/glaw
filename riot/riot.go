3b14551b64620c01ccd5cb1edad32a7f9618eab5
Colton Roebrtson <colton.robertson@rewardstyle.com>
2017-08-05 15:41:21 -0500

    Using one package for the api instead of many
    Instead of making small mini packages everywhere. Using one big package
    to get the api results

--- riot/riot.go
+++ riot/riot.go
@@ -3,7 +3,10 @@ package riot
 import (
     "fmt"
     "io/ioutil"
+    "log"
     "net/http"
+
+    "github.com/julienschmidt/httprouter"
 )
 import c "vs/config"
 
@@ -12,6 +15,8 @@ const Url = "https://na1.api.riotgames.com/lol"
 var config = c.GetConfig()
 var Version = config.Version
 
+type apiNoParams func() ([]byte, error)
+type apiParams func(*httprouter.Params) ([]byte, error)
 
 func add_headers(req *http.Request) {
     req.Header.Add("X-Riot-Token", config.Api.Token)
@@ -21,6 +26,11 @@ func add_headers(req *http.Request) {
 // Not sure if this is the best practice
 var Client = &http.Client{
 //    CheckRedirect: redirectPolicyFunc,
+}
+
+func BuildUrls(router *httprouter.Router) {
+    // Bread and butter of the package
+    mastery_init(router)
 }
 
 func GetData(http_method string, uri string) ([]byte, error) {
@@ -36,4 +46,26 @@ func GetData(http_method string, uri string) ([]byte, error) {
     body, _ := ioutil.ReadAll(resp.Body)
 
     return body, err
+}
+
+func noParams(fn apiNoParams) (httprouter.Handle) {
+    return func(w http.ResponseWriter, r *http.Request,  _ httprouter.Params) {
+        body, err := fn()
+        if err != nil {
+            log.Fatalf("DefaultOutputHandler found an err: %v", err)
+        }
+
+        w.Write(body)
+    }
+}
+
+func hasParams(fn apiParams) (httprouter.Handle) {
+    return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+        body, err := fn(&ps)
+        if err != nil {
+            log.Fatalf("DefaultOutputHandler found an err: %v", err)
+        }
+
+        w.Write(body)
+    }
 }
